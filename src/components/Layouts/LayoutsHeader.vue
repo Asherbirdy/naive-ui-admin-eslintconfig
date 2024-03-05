eb52f9d20f6c3c28ab99aafad74cdb45b77e8e0e
Asher Lien <asher.lien@riversoft.com.tw>
2024-03-05 16:39:57 +0800

    切版

--- src/components/Layouts/LayoutsHeader.vue
+++ src/components/Layouts/LayoutsHeader.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
-import {
-  NLayout,
-  NLayoutContent
- } from 'naive-ui'
+import { NLayout, NLayoutContent, NButton, NFlex, NH2 } from 'naive-ui'
+
+const handleLogout = () => {
+  console.log('logout')
+}
 </script>
 <template>
   <div>
     <n-layout>
       <n-layout-content class="p-10">
-        Header
+        <n-flex justify="space-between">
+          <n-h2>
+            logo
+          </n-h2>
+          <n-button @click="handleLogout" size="tiny">
+            logout
+          </n-button>
+        </n-flex>
       </n-layout-content>
     </n-layout>
   </div>
