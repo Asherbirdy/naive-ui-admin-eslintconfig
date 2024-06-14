63ecaab3d11d2692942dbaaa6488cca14643f880
Asher Lien <asher.lien@riversoft.com.tw>
2024-06-14 14:59:58 +0800

    更新資料

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,17 +4,10 @@
 
 <script setup lang='ts'>
 
-const userId = ref('')
 </script>
 
 <template>
   <div>
-    <router-link to="./login">
-      <button>login</button>
-    </router-link>
-    <button>
-      Current User
-    </button>
-    <p>{{ userId }}</p>
+    hi
   </div>
 </template>
